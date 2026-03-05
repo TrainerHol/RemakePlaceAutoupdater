c0589593dd2f93cd4c93e301f16a5643f543dda3
Lovász Ákos <akos.lovasz@chameleon.sh>
2026-03-05 19:31:44 +0100

    fix multiple icons appearing after update

--- ReMakeplaceUpdater/src/main.ts
+++ ReMakeplaceUpdater/src/main.ts
@@ -628,6 +628,11 @@ class ReMakeplaceUpdater {
     link.onmouseenter = () => link.style.opacity = "0.6";
     link.onmouseleave = () => link.style.opacity = "1";
     link.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="16" viewBox="0 0 24 24" width="24" style="fill: white;"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/></svg>`;
+
+    if (this.releaseNotesElement.children.length > 0){
+      this.releaseNotesElement.innerHTML = "";
+    }
+
     this.releaseNotesElement.appendChild(link);
   }
 
